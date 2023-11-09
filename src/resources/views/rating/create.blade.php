16781f04906d06b2b521aef4f38a46d37a1966be
thanhdeptrai <ducthanh.1708.2001@gmail.com>
2023-11-10 00:06:53 +0700

    update css

--- src/resources/views/rating/create.blade.php
+++ src/resources/views/rating/create.blade.php
@@ -1,117 +1,221 @@
-@extends('layouts.layouts')
-@section('content')
-<link rel="stylesheet" href="{{ asset('css/allrating.css') }}" />
+<!DOCTYPE html>
+<html>
 
-<form action="create-criterias" method="post">
-  @csrf
-  <div class="frame-parent10">
-    <div class="instance-parent">
-      <div class="button-muangay-wrapper">
-        <div class="button-muangay">
-          <input type="submit" value="Submit" class="lu-thay-i">
-        </div>
-      </div>
-      <div class="group-div">
-        <div class="frame-parent11">
-          <div class="frame-parent12">
-            <div class="tiu-wrapper">
-              <div class="tiu">Tiêu đề</div>
-            </div>
-            <div class="frame-parent13">
-              <div class="frame-item"></div>
-              <div class="frame-child1"></div>
-              <input type="text" class="trng-mi" id="name" name="name" placeholder="trường mới" required>
-            </div>
-          </div>
-          <div class="frame-parent14">
-            <div class="tiu-wrapper">
-              <div class="tiu">Thuôc trường</div>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="initial-scale=1, width=device-width" />
 
-            </div>
-            <div class="frame-parent15">
-              <div class="frame-parent16">
-                <div class="vui-lng-nhp-phngx-wrapper">
-                  <select class="thng-tin-lin" name="parent_criteria_id" id="parent_criteria_id">
-                    @if($par)
-                    <option value="{{$par['id']}}">{{$par['name']}}</option>
-                    @else
-                    <option value="NULL" selected>Không thuộc trường nào</option>
-                    @endif
-                    @foreach($critetias as $value)
-                    @if($value['field_level'] != 3)
-                    <option value="{{$value['id']}}">{{$value['name']}}</option>
-                    @endif
-                    @endforeach
-                  </select>
-                </div>
-                <div class="icround-navigate-next-wrapper">
-                  <img class="location-on-icon" alt="" src="./public/icroundnavigatenext.svg" />
-                </div>
-              </div>
+  <link rel="stylesheet" href="{{ asset('css/global.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/addfield.css') }}" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" />
+</head>
+
+<body>
+  <div class="frame">
+    <div class="foodter">
+      <img class="image-56-icon" alt="" src="./public/image-56@2x.png" />
+
+      <div class="thng-tin-lin-h-parent">
+        <div class="thng-tin-lin">Thông tin liên hệ</div>
+        <div class="frame-parent">
+          <div class="location-on-parent">
+            <img class="location-on-icon" alt="" src="./public/location-on.svg" />
+
+            <div class="email-daihocsaigonsgueduvn">
+              Địa chỉ: 273 Đ. An D. Vương, Phường 3, Quận 5, Thành phố Hồ Chí
+              Minh 700000, Việt Nam
             </div>
           </div>
-          <div class="frame-parent14">
-            <div class="tiu-wrapper">
-              <div class="tiu">Điểm tối đa</div>
-            </div>
-            <div class="frame-parent19">
-              <div class="frame-parent16">
-                <div class="vui-lng-nhp-phngx-wrapper">
-                  {{-- <div class="vui-lng-nhp1">
-                        diwajdAAAdadmindiwajdAAAdadmin
-                      </div> --}}
-                  <input type="number" class="vui-lng-nhp1" id="max_score" name="max_score" min="1" max="30" required>
-                </div>
-                <div class="icround-navigate-next-wrapper">
-                  <img class="location-on-icon" alt="" src="./public/icroundnavigatenext1.svg" />
-                </div>
-              </div>
+          <div class="email-parent">
+            <img class="location-on-icon" alt="" src="./public/email.svg" />
+
+            <div class="email-daihocsaigonsgueduvn">
+              <a class="daihocsaigonsgueduvn" href="mailto:daihocsaigon@sgu.edu.vn" target="_blank">daihocsaigon@sgu.edu.vn</a>
             </div>
           </div>
-          <div class="frame-parent22">
-            <div class="tiu-wrapper">
-              <div class="tiu">Điểm mạc định</div>
-            </div>
-            <div class="frame-parent19">
-              <div class="frame-parent16">
-                <div class="vui-lng-nhp-phngx-wrapper">
-                  {{-- <div class="vui-lng-nhp1">1,2,3,4,5,6,,77</div> --}}
-                  <input type="number" class="vui-lng-nhp1" id="default_score" name="default_score" min="1" max="30" required>
-                </div>
-                <div class="icround-navigate-next-frame">
-                  <img class="location-on-icon" alt="" src="./public/icroundnavigatenext2.svg" />
-                </div>
-              </div>
+          <div class="language-parent">
+            <img class="location-on-icon" alt="" src="./public/language.svg" />
+
+            <div class="email-daihocsaigonsgueduvn">
+              Website: https://www.sgu.edu.vn/
             </div>
           </div>
-          <div class="frame-parent14">
-            <div class="tiu-wrapper">
-              <div class="tiu">Loại điểm</div>
-            </div>
-            <div class="frame-parent19">
-              <div class="frame-parent16">
-                <div class="vui-lng-nhp-phngx-wrapper">
-                  {{-- <div class="vui-lng-nhp1">
-                        diwajdAAAdadmindiwajdAAAdadmin
-                      </div> --}}
-                  <select class="vui-lng-nhp1" name="is_violent" id="is_violent">
-                    <option value="0">Điểm cộng</option>
-                    <option value="1">Điểm trừ</option>
-                  </select>
-                </div>
-                <div class="icround-navigate-next-wrapper">
-                  <img class="location-on-icon" alt="" src="./public/icroundnavigatenext3.svg" />
-                </div>
-              </div>
+          <div class="language-parent">
+            <img class="location-on-icon" alt="" src="./public/phone.svg" />
+
+            <div class="email-daihocsaigonsgueduvn">
+              Hotline: (84-28) 38.354409 - 38.352309
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="thm-trng-wrapper">
-      <div class="thm-trng">Thêm trường</div>
+    <div class="header">
+      <div class="frame-group">
+        <div class="frame-wrapper">
+          <div></div>
+        </div>
+        <div class="trng-i-hc-si-gn-parent">
+          <b class="trng-i-hc">TRƯỜNG ĐẠI HỌC SÀI GÒN</b>
+          <b class="trng-i-hc1">SINCE 2007</b>
+        </div>
+      </div>
+      <div class="image-55-wrapper">
+        <img class="image-55-icon" alt="" src="./public/image-55@2x.png" />
+      </div>
     </div>
+    <div class="frame-inner">
+      <div class="frame-container">
+        <div class="rectangle-parent">
+          <img class="frame-child" alt="" src="./public/rectangle-4554@2x.png" />
+
+          <div class="admin-minh-thc-wrapper">
+            <div class="admin-minh-thc">Admin Minh Thức</div>
+          </div>
+        </div>
+        <div class="instance-wrapper">
+          <div class="frame-div">
+            <div class="danh-sch-ng-vin-wrapper">
+              <div class="danh-sch-ng">Người dùng</div>
+            </div>
+            <img class="location-on-icon" alt="" src="./public/expand-more.svg" />
+          </div>
+        </div>
+        <div class="instance-container">
+          <div class="frame-parent1">
+            <div class="danh-sch-ng-vin-wrapper">
+              <div class="danh-sch-ng1">Hồ sơ cá nhân</div>
+            </div>
+            <img class="expand-more-icon1" alt="" src="./public/expand-more1.svg" />
+          </div>
+        </div>
+        <div class="instance-wrapper">
+          <div class="frame-div">
+            <div class="danh-sch-ng-vin-wrapper">
+              <div class="danh-sch-ng">Các đánh giá</div>
+            </div>
+            <img class="location-on-icon" alt="" src="./public/expand-more2.svg" />
+          </div>
+        </div>
+        <div class="instance-container">
+          <div class="frame-parent1">
+            <div class="danh-sch-ng-vin-wrapper">
+              <div class="danh-sch-ng1">Sinh viên</div>
+            </div>
+            <img class="expand-more-icon1" alt="" src="./public/expand-more3.svg" />
+          </div>
+        </div>
+        <div class="instance-container">
+          <div class="frame-parent1">
+            <div class="danh-sch-ng-vin-wrapper">
+              <div class="danh-sch-ng1">Tất cả đánh giá</div>
+            </div>
+            <img class="expand-more-icon1" alt="" src="./public/expand-more4.svg" />
+          </div>
+        </div>
+        <div class="instance-wrapper">
+          <div class="frame-div">
+            <div class="danh-sch-ng-vin-wrapper">
+              <div class="danh-sch-ng">Thông tin hỗ trợ</div>
+            </div>
+            <img class="location-on-icon" alt="" src="./public/expand-more5.svg" />
+          </div>
+        </div>
+        <div class="instance-wrapper">
+          <div class="frame-div">
+            <div class="danh-sch-ng-vin-wrapper">
+              <div class="danh-sch-ng">Tin tức</div>
+            </div>
+            <img class="location-on-icon" alt="" src="./public/expand-more6.svg" />
+          </div>
+        </div>
+        <div class="instance-wrapper">
+          <div class="frame-div">
+            <div class="danh-sch-ng-vin-wrapper">
+              <div class="danh-sch-ng">Thông báo</div>
+            </div>
+            <img class="location-on-icon" alt="" src="./public/expand-more7.svg" />
+          </div>
+        </div>
+        <div class="frame-parent8">
+          <div class="danh-sch-ng-vin-wrapper">
+            <div class="danh-sch-ng1">Tất cả thông báo</div>
+          </div>
+          <img class="expand-more-icon1" alt="" src="./public/expand-more8.svg" />
+        </div>
+        <div class="instance-wrapper6">
+          <div class="frame-parent9">
+            <div class="danh-sch-ng-vin-wrapper">
+              <div class="danh-sch-ng1">Thêm mới thông báo</div>
+            </div>
+            <img class="expand-more-icon1" alt="" src="./public/expand-more9.svg" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <form action="create-criterias" method="post">
+      @csrf
+      <div class="frame-parent10">
+        <div class="instance-parent">
+
+          <div class="group-div">
+            <div class="button-muangay-wrapper">
+              <input type="submit" value="Submit" class="button-muangay">
+            </div>
+            <div class="frame-parent11">
+              <div class="frame-parent14">
+                <div class="tiu-wrapper">
+                  <div class="tiu">Tiêu đề</div>
+                </div>
+                <input type="text" class="frame-parent13" id="name" name="name" placeholder="trường mới" required>
+              </div>
+              <div class="frame-parent14">
+                <div class="tiu-wrapper">
+                  <div class="tiu">Thuôc trường</div>
+                </div>
+                <select class="frame-parent13" name="parent_criteria_id" id="parent_criteria_id">
+                  @if($par)
+                  <option value="{{$par['id']}}">{{$par['name']}}</option>
+                  @else
+                  <option value="NULL" selected>Không thuộc trường nào</option>
+                  @endif
+                  @foreach($critetias as $value)
+                  @if($value['field_level'] != 3)
+                  <option value="{{$value['id']}}">{{$value['name']}}</option>
+                  @endif
+                  @endforeach
+                </select>
+              </div>
+              <div class="frame-parent14">
+                <div class="tiu-wrapper">
+                  <div class="tiu">Điểm tối đa</div>
+                </div>
+                <input type="number" class="frame-parent13" id="max_score" name="max_score" min="1" max="30" required>
+              </div>
+              <div class="frame-parent14">
+                <div class="tiu-wrapper">
+                  <div class="tiu">Điểm mạc định</div>
+                </div>
+                <input type="number" class="frame-parent13" id="default_score" name="default_score" min="1" max="30" required>
+              </div>
+              <div class="frame-parent14">
+                <div class="tiu-wrapper">
+                  <div class="tiu">Loại điểm</div>
+                </div>
+                <select class="frame-parent13" name="is_violent" id="is_violent">
+                  <option value="0">Điểm cộng</option>
+                  <option value="1">Điểm trừ</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="thm-trng-wrapper">
+          <div class="thm-trng">Thêm trường</div>
+        </div>
+      </div>
+    </form>
   </div>
-</form>
+</body>
 
-@endsection
+</html>
